3ebbf989fcde2e6e1b47dedfed650c220c4c259d
Kumar Aditya <kumaraditya1814@gmail.com>
2025-10-06 22:02:08 +0530

    implimented header

---
+++ app/(root)/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import {Button} from "@/components/ui/button";
+
+const Home = () => {
+    return (
+        <div className="min-h-screen home-wrapper">
+            Home
+
+        </div>
+    )
+}
+export default Home
